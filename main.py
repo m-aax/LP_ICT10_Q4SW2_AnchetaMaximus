4c15b4917ce1a26d05fcbb9d7c4f2d664dcfbca2
m-aax <ancheta.aea@obmontessori.edu.ph>
2026-04-13 14:21:51 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+classmates = [
+    {"name": "Max Ancheta", "place": "New York"},
+    {"name": "Alonso Asuncion", "place": "Heaven"},
+    {"name": "Enzo Battung", "place": "Spain"},
+    {"name": "Athena Catapang", "place": "Coachella"},
+    {"name": "Cade Chua", "place": "Japan"},
+    {"name": "Zyan Eusebio", "place": "Heaven"},
+    {"name": "Rad Evangelio", "place": "Houston, TX"},
+    {"name": "Mara Fado", "place": "Japan"},
+    {"name": "Curt Fernando", "place": "Bali"},
+    {"name": "Kleiser Fermocil", "place": "Canada"},
+    {"name": "Ethan Francia", "place": "Netherlands"},
+    {"name": "Sophia Jimenez", "place": "London"},
+    {"name": "AC Mactal", "place": "Japan"},
+    {"name": "Soleil Magday", "place": "Anywhere"},
+    {"name": "Javi Mabilog", "place": "Disneyworld"},
+    {"name": "Yanna Moya", "place": "Italy"},
+    {"name": "Zoe Mutia", "place": "Vienna, Austria"},
+    {"name": "Luis Nazareno", "place": "Hawaii"},
+    {"name": "Inigo Romero", "place": "Nigeria"},
+    {"name": "Jaedin Sarao", "place": "Switzerland"},
+    {"name": "Kyler Santos", "place": "Krusty Krab"},
+    {"name": "Charlotte Sy", "place": "Coachella"},
+    {"name": "Briana Sy", "place": "Rome"},
+    {"name": "Jared Udono", "place": "Japan"},
+    {"name": "KC Vida", "place": "Monaco"}
+]
